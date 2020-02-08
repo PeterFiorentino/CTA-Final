d22e957dc57fdc3ee3164e7e22466294c2d02433
PeterFiorentino <peterfiorentino@pursuit.org>
2020-02-08 13:21:12 -0500

    created a basic Show page

---
+++ cta_front_end/src/Components/Shows.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Shows = () => {
+    return(
+        <div>
+            <h1> Shows Page</h1>
+        </div>
+    )
+}
+
+export default Shows;
