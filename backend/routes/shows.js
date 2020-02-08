2cec9f7d47ebc0f2c0a2074691199335bc6e6203
PeterFiorentino <peterfiorentino@pursuit.org>
2020-02-08 12:22:30 -0500

    made a get a single user route

--- backend/routes/shows.js
+++ backend/routes/shows.js
@@ -19,4 +19,22 @@ router.get('/', async (req, res) => {
     }
   });
 
+router.get('/:id', async (req, res) => {
+    try {
+        let singleShow = await db.one(`SELECT * FROM shows WHERE id = $1`, [req.params.id])
+        res.json({
+        status: "Success",
+        body: {
+            shows: singleShow
+        }
+        })
+    } catch (error) {
+        res.json({
+        message: "There was a problem getting a show with that ID.",
+        err: error
+        })
+    }
+  });
+
+
 module.exports = router;
