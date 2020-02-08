c0e556eeec097bcb2b31e7f266b66ddf2641e4c5
PeterFiorentino <peterfiorentino@pursuit.org>
2020-02-08 12:31:22 -0500

    created a post a show route

--- backend/routes/shows.js
+++ backend/routes/shows.js
@@ -36,5 +36,32 @@ router.get('/:id', async (req, res) => {
     }
   });
 
+router.post('/', async (req, res) => {
+    try {
+        let insertQuery =`INSERT INTO shows(title, img_url, user_id, genre_id) 
+        VALUES($1, $2, $3, $4)`
+  
+        if(!req.body.title || !req.body.img_url || !req.body.user_id || !req.body.genre_id){
+            res.json({
+                "message": "Information Missing"
+            })
+        }else {
+            await db.none(insertQuery, [req.body.title, req.body.img_url, req.body.user_id, req.body.genre_id]);
+  
+            res.json({
+                title: req.body.title,
+                img_url: req.body.img_url,
+                user_id: req.body.user_id,
+                genre_id: req.body.genre_id,
+                message: `Posted ${req.body.title} successfully.`
+            })
+        }
+    } catch(error) {
+        res.json({
+            message: error
+        })
+    }
+  });
+  
 
 module.exports = router;
