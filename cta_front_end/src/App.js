ec8489a19b999dd88232269fc4d2d2d963776f2c
PeterFiorentino <peterfiorentino@pursuit.org>
2020-02-08 13:08:50 -0500

    Created a NavBar

--- cta_front_end/src/App.js
+++ cta_front_end/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar'
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+
+      <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/about" component={About}/>
+              <Route exact path="/users" component={Users}/>
+              <Route exact path="/shows" component={Shows}/>
+      </Switch>
     </div>
   );
 }
--- cta_front_end/src/Components/NavBar.jsx
+++ cta_front_end/src/Components/NavBar.jsx
@@ -1,3 +1,17 @@
 import React from 'react'
-import { Link, Route, Switch } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <strong><p>APP NAME</p></strong>
+            <Link to="/" >Home</Link>
+            <Link to="/users" >Users</Link>
+            <Link to="/shows" >Shows</Link>
+            <Link to="/about" >About</Link>
+        </nav>
+    )
+}
+
+export default NavBar;
 
