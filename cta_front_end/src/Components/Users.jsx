7014077c3551d4a8d608209a15ac27461c94b0d3
PeterFiorentino <peterfiorentino@pursuit.org>
2020-02-08 13:21:38 -0500

    created a basic user page

---
+++ cta_front_end/src/Components/Users.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Users = () => {
+    return(
+        <div>
+            <h1> Users Page</h1>
+        </div>
+    )
+}
+
+export default Users;
